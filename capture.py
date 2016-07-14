f16df663b303a92f4decd0b824a3c396501ea3b3
Bryce McDonnell <bryce@bridgetownint.com>
2016-07-14 00:13:14 +0000

    update timestamp

--- capture.py
+++ capture.py
@@ -1,17 +1,23 @@
 from time import sleep
 from picamera import PiCamera
 
-cam = PiCamera(resolution=(1280, 720), framerate=60)
+high=(3264,2448)
+mid=(1024,768)
+low=(320,240)
+
+cam = PiCamera(resolution=mid, framerate=60)
+
 #cam.awb_mode = 'off'
 cam.meter_mode = 'matrix'
-cam.shutter_speed = int((1/60.0) * 1000000)
-cam.vflip = True
-cam.iso = 64
+#cam.exposure_mode = 'night'
+#cam.shutter_speed = int((1/12.0) * 1000000)
+#cam.vflip = True
+#cam.iso = 32 
 
-#cam.start_preview()
-#sleep(2)
-#cam.stop_preview()
+cam.start_preview()
+sleep(2)
+cam.stop_preview()
 
-for filename in cam.capture_continuous('data/img-{timestamp:%Y:%m:%d::%H:%M:%s}.jpg'):
+for filename in cam.capture_continuous('/home/pi/Desktop/cam/data/img-{timestamp:%Y-%m-%dT%H:%M:%S.%f%z}.jpg'):
   print('Captured %s' % filename)
-  sleep(30)
+  sleep(10)
