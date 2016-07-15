ede92cf3442473829699871f0e4188cb217ba0ac
Bryce McDonnell <bryce@bridgetownint.com>
2016-07-15 00:00:05 +0000

    potential fix to strange timestamp issue

--- capture.py
+++ capture.py
@@ -1,11 +1,13 @@
 from time import sleep
 from picamera import PiCamera
+import string
+import random
 
 high=(3264,2448)
 mid=(1024,768)
 low=(320,240)
 
-cam = PiCamera(resolution=mid, framerate=60)
+cam = PiCamera(resolution=mid)
 
 #cam.awb_mode = 'off'
 cam.meter_mode = 'matrix'
@@ -18,6 +20,10 @@ cam.start_preview()
 sleep(2)
 cam.stop_preview()
 
-for filename in cam.capture_continuous('/home/pi/Desktop/cam/data/img-{timestamp:%Y-%m-%dT%H:%M:%S.%f%z}.jpg'):
+seed = string.ascii_lowercase + string.digits
+random_str = ''.join(random.choice(seed) for _ in range(6))
+filename_prefix = '/home/pi/Desktop/cam/data/img-%s' % random_str
+
+for filename in cam.capture_continuous(filename_prefix + "{timestamp:%Y-%m-%dT%H:%M:%S.%f%Z}-{counter:05d}.jpg"):
   print('Captured %s' % filename)
   sleep(10)
